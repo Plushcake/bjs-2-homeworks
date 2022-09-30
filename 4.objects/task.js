2291fcdd6ab5501a1ff09b7d6dad198c226da7e7
Plushcake <Nikkolo.Makeawelli@gmail.com>
2022-09-30 13:36:00 +0300

    modified task.js v.1.6

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -3,20 +3,37 @@ function Student(name, gender, age) {
   this.gender = gender;
   this.age = age;
 }
-let student1 = new Student("Вася", "Мужской", 20);
-let student2 = new Student("Света", "Женский", 19);
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 Student.prototype.addMark = function (mark) {
   if (this.marks === undefined) {
-    this.marks = mark;
+    this.marks = [mark];
   } else {
-
+    this.marks.push(mark);
   }
-  Student.prototype.addMarks = function (...mark) {
-
-  }
-
 }
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = [...mark];
+  } else {
+    this.marks.push(...mark);
+  }
+}
+Student.prototype.getAverage = function () {
+  let acc = 0;
+  for (i = 0; i < this.marks.length; i++) {
+    acc = acc + this.marks[i];
+  }
+  return acc / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
+
